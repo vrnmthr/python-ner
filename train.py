603e6ca48293a4f8dbc46a8fc494595f95aee42e
Varun Mathur <varun.mathur@live.com>
2019-08-09 07:34:11 -0400

    made all words lowercase

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def get_label(x):
 def get_words_and_tags(sentence):
     # extracts tags and words from CONLL formatted entry
     tags = list(map(lambda word: get_label(word[1]), sentence))
-    words = list(map(lambda word: word[0][0], sentence))
+    words = list(map(lambda word: word[0][0].strip().lower(), sentence))
     return words, tags
 
 
