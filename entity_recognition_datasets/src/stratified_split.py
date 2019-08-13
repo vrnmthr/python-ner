414b1f925929ff7af6722143c6676af26dd5aece
Varun Mathur <varun.mathur@live.com>
2019-08-13 09:35:16 -0400

    added test/train split of BTC

--- entity_recognition_datasets/src/stratified_split.py
+++ entity_recognition_datasets/src/stratified_split.py
@@ -4,16 +4,18 @@ level) for those corpora which do not have test/train splits, and for saving
 the test and train data in CONLL 2003 format in separate directories.
 
 """
+import codecs
 import os
 import sys
-import codecs
+
 from sklearn.model_selection import train_test_split
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import utils
 
+
 def write_new_split(corpus_name, test_size, filedir, filename,
-                    seed = 42, max_count = 2):
+                    seed=42, max_count=2):
     """ Do stratified random sampling for a given corpus given by corpus_name,
     and save the results in file filename in directory filedir. Parameter
     test_size indicates the number of sentences to be used in the test set.
@@ -36,16 +38,16 @@ def write_new_split(corpus_name, test_size, filedir, filename,
     sentences = list(r)
     train_data, test_data = stratified_split(sentences,
                                              test_size,
-                                             seed = seed,
-                                             max_count = max_count)
+                                             seed=seed,
+                                             max_count=max_count)
 
-    writefile(train_data, os.path.join(filedir,'train'), filename+'-train.conll')
-    writefile(test_data, os.path.join(filedir,'test'), filename+'-test.conll')
+    writefile(train_data, os.path.join(filedir, 'train'), filename + '-train.conll')
+    writefile(test_data, os.path.join(filedir, 'test'), filename + '-test.conll')
 
     return train_data, test_data
 
 
-#TODO this should be moved to utils.
+# TODO this should be moved to utils.
 def writefile(sentences, filedir, filename, sep='\t'):
     """ Write the sentences, in CONLL-format, to a file given by filename
     located in directory filedir.
@@ -54,17 +56,17 @@ def writefile(sentences, filedir, filename, sep='\t'):
     DIR = filedir
     WRITEFILE = os.path.join(DIR, filename)
     if not os.path.exists(DIR):
-            os.makedirs(DIR)
+        os.makedirs(DIR)
     if os.path.isfile(WRITEFILE):
         raise ValueError("The file already exists!")
-    with codecs.open(WRITEFILE,'a+',encoding='utf-8') as fd:
+    with codecs.open(WRITEFILE, 'a+', encoding='utf-8') as fd:
         for sent in sentences:
             fd.write('\n')
             for tok in sent:
-                fd.write(tok[0][0] + sep + tok[1]+'\n')
+                fd.write(tok[0][0] + sep + tok[1] + '\n')
 
 
-def stratified_split(sentences, test_size, max_count = 2, seed = 42):
+def stratified_split(sentences, test_size, max_count=2, seed=42):
     """
     Uses 'train_test_split' with stratification from scikit learn. However,
     since we want to split on the sentences level, we first map label
@@ -80,8 +82,8 @@ def stratified_split(sentences, test_size, max_count = 2, seed = 42):
     event the element is mapped to the class of sentences with no labels.
 
     """
-    labels = [[iob for (x,iob) in d] for d in sentences]
-    tags = list(set([i for s in labels for i in s if i[0]=='B']))
+    labels = [[iob for (x, iob) in d] for d in sentences]
+    tags = list(set([i for s in labels for i in s if i[0] == 'B']))
     inds = range(len(labels))
 
     Y = [_map_labels(l, tags, max_count) for l in labels]
@@ -89,12 +91,12 @@ def stratified_split(sentences, test_size, max_count = 2, seed = 42):
     # Quick fix: if any element in Y only appears once, change it to 0
     ##################################################################
     if len(tags) < 20:
-        maxnum = (max_count+1)**len(tags) - 1
-        Yfreq = [Y.count(i) for i in range(maxnum+1)]
-        fixcounts = [i for i,x in enumerate(Yfreq) if x==1]
+        maxnum = (max_count + 1) ** len(tags) - 1
+        Yfreq = [Y.count(i) for i in range(maxnum + 1)]
+        fixcounts = [i for i, x in enumerate(Yfreq) if x == 1]
         for c in fixcounts:
             Y[Y.index(c)] = 0
-    else: # Due to memory issues:
+    else:  # Due to memory issues:
         cc = [Y.count(Y[i]) for i in range(len(Y))]
         for i in range(len(cc)):
             if cc[i] == 1:
@@ -102,7 +104,7 @@ def stratified_split(sentences, test_size, max_count = 2, seed = 42):
     ###################################################################
 
     inds_train, inds_test, Y_train, Y_test = train_test_split(inds, Y,
-        test_size = test_size, random_state=seed, stratify=Y)
+                                                              test_size=test_size, random_state=seed, stratify=Y)
 
     train_data = [sentences[i] for i in inds_train]
     test_data = [sentences[i] for i in inds_test]
@@ -116,18 +118,18 @@ def check_label_ratios(train_data, test_data, orig_data, tags):
     entity class tags to compare.
 
     """
-    train_tagcounts =  [ sum([len([i for i in T if i[1] == tag]) for T in train_data]) for tag in tags]
-    test_tagcounts =  [ sum([len([i for i in T if i[1] == tag]) for T in test_data]) for tag in tags]
-    orig_tagcounts =  [ sum([len([i for i in T if i[1] == tag]) for T in orig_data]) for tag in tags]
+    train_tagcounts = [sum([len([i for i in T if i[1] == tag]) for T in train_data]) for tag in tags]
+    test_tagcounts = [sum([len([i for i in T if i[1] == tag]) for T in test_data]) for tag in tags]
+    orig_tagcounts = [sum([len([i for i in T if i[1] == tag]) for T in orig_data]) for tag in tags]
 
-    train_ratios = [float(i)/sum(train_tagcounts) for i in train_tagcounts]
-    test_ratios = [float(i)/sum(test_tagcounts) for i in test_tagcounts]
-    orig_ratios = [float(i)/sum(orig_tagcounts) for i in orig_tagcounts]
+    train_ratios = [float(i) / sum(train_tagcounts) for i in train_tagcounts]
+    test_ratios = [float(i) / sum(test_tagcounts) for i in test_tagcounts]
+    orig_ratios = [float(i) / sum(orig_tagcounts) for i in orig_tagcounts]
 
     return train_ratios, test_ratios, orig_ratios
 
 
-def _map_labels(labels, tags, max_count = 2):
+def _map_labels(labels, tags, max_count=2):
     """
 
     Parameters
@@ -145,7 +147,5 @@ def _map_labels(labels, tags, max_count = 2):
         if counts[i] > max_count:
             counts[i] = max_count
 
-    Y = int("".join(map(str, counts)), base = base)
+    Y = int("".join(map(str, counts)), base=base)
     return Y
-
-
